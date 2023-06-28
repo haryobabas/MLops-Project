1264f33a57921528cb98d6a29bc9e04ff5e21ffa
haryobabas <haryo.bbas1@gmail.com>
2023-06-28 22:28:31 +0700

    Finishing & add dockerfile

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+import pickle
 
 app = FastAPI()
+
+class TextInput(BaseModel):
+    text: str
+
+model=pickle.load(open("spam.pkl", "rb"))
+cv=pickle.load(open("vectorizer.pkl","rb")) 
+
 
 @app.get("/")
 def index():
@@ -10,6 +20,17 @@ def index():
             "message": "Success fetching the API"
         }
     }
+
+@app.post("/predict")
+def predict_spam(text_input: TextInput):
+    data=[text_input.text]
+    vec=cv.transform(data).toarray()
+    result=model.predict(vec)
+    if result[0]==0:
+        return {"prediction": "This is not spam email"}
+    else:
+        return {"prediction": "This is a spam email"}
+
 
 if __name__ == "__main__":
     import uvicorn
