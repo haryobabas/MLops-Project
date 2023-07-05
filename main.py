44426cd74c5f7bda6853db210e578600a71ddef8
haryobabas <haryo.bbas1@gmail.com>
2023-07-05 22:17:22 +0700

    Fixing Issue Deployment

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -34,4 +35,6 @@ def predict_spam(text_input: TextInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, 
+                host="0.0.0.0", 
+                port=int(os.environ.get("PORT", 8080)))
